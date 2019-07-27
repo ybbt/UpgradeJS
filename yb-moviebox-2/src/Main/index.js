e5021997f0456c65a9b7c74f8eeaf5f125e25e0e
ybbt <yorik_boss@ukr.net>
2019-07-27 08:51:22 +0300

    progress

--- yb-moviebox-2/src/Main/index.js
+++ yb-moviebox-2/src/Main/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { BrowserRouter } from 'react-router-dom';
+
 import Film from '../Film';
 import Pagenation from '../Pagenation';
 
@@ -13,42 +15,80 @@ class Main extends React.Component{
     genres: {},
   };
 
-  componentDidMount() {
-    fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=399a504355fb64900d932566782c9bb5&language=uk-UA&page=1&region=UA')
+  ApiService(responseString, setStateFunc){
+    // console.log('hello');
+    fetch(responseString)
       .then(
         response =>
         response.ok ? response.json() : Promise.reject(Error('Failed to load'))
       )
       .then(
-        result => {
-          this.setState({
-            movies: result.results,
-            pages: result.total_pages,
-          });
-
-          // console.log(this.state.movies);
-        },
-        error => console.log(error)
-      );
-
-      fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=399a504355fb64900d932566782c9bb5&language=uk-UA')
-      .then(
-        response =>
-        response.ok ? response.json() : Promise.reject(Error('Failed to load'))
-      )
-      .then(
-        result => {
-          let temp = {};
-          result.genres.map(item => temp[item.id] = item.name);
-          this.setState({
-            genres: temp,
-          });
-        },
+        /* this.setStateMovies.bind(this) */setStateFunc,
         error => console.log(error)
       );
   }
-    render(){
-      return (
+
+  setStateMovies(result){
+    this.setState({
+      movies: result.results,
+      pages: result.total_pages,
+    });
+  }
+
+  setStateGenres(result){
+    let temp = {};
+    result.genres.map(item => temp[item.id] = item.name);
+    this.setState({
+      genres: temp,
+    });
+  }
+
+  componentDidMount() {
+    // fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=399a504355fb64900d932566782c9bb5&language=uk-UA&page=1&region=UA')
+    //   .then(
+    //     response =>
+    //     response.ok ? response.json() : Promise.reject(Error('Failed to load'))
+    //   )
+    //   .then(
+    //     result => {
+    //       this.setState({
+    //         movies: result.results,
+    //         pages: result.total_pages,
+    //       });
+
+    //       // console.log(this.state.movies);
+    //     },
+    //     error => console.log(error)
+    //   );
+    this.ApiService('https://api.themoviedb.org/3/movie/now_playing?api_key=399a504355fb64900d932566782c9bb5&language=uk-UA&page=1&region=UA', this.setStateMovies.bind(this));
+
+    // fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=399a504355fb64900d932566782c9bb5&language=uk-UA')
+    //   .then(
+    //     response => response.ok ? response.json() : Promise.reject(Error('Failed to load'))
+    //   )
+    //   .then(
+    //     result => {
+    //       let temp = {};
+    //       result.genres.map(item => temp[item.id] = item.name);
+    //       this.setState({
+    //         genres: temp,
+    //       });
+    //     },
+    //     error => console.log(error)
+    //   );
+
+    this.ApiService('https://api.themoviedb.org/3/genre/movie/list?api_key=399a504355fb64900d932566782c9bb5&language=uk-UA', this.setStateGenres.bind(this));
+  }
+
+  
+
+  render(){
+    let pagesArr = [];
+    for (let index = 1; index <= this.state.pages; index++) {
+      pagesArr.push(index);
+    }
+    return (
+      <BrowserRouter>
         <div >
           <div className={style.films}>
             {this.state.movies.map(item => {
@@ -57,10 +97,12 @@ class Main extends React.Component{
               )
             })}
           </div>
-          <Pagenation pages={this.state.pages} />
+          <Pagenation pages={pagesArr} />
         </div>
-      );
-      
-    }
+      </BrowserRouter>
+    );
+    
   }
-  export default Main;
+}
+export default Main;
+
--- yb-moviebox-2/src/Pagenation/index.js
+++ yb-moviebox-2/src/Pagenation/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
+import { Route, Link } from "react-router-dom";
+
 import style from './style.module.css'
 
 function Pagenation(props) {
-    console.log(props.pages);
-    let pagesArr = [];
-    for (let index = 1; index <= props.pages; index++) {
-        pagesArr.push(index);
-        
-    }
-    console.log(pagesArr);
+    // console.log(props.pages);
+    // let pagesArr = [];
+    // for (let index = 1; index <= props.pages; index++) {
+    //     pagesArr.push(index);
+    // }
+    // console.log(pagesArr);
     return (
       <div>
-        {pagesArr.map((item, index) => {return <a href="./" key={index} className={style.pages}>{item}</a>})}
+        {/* {pagesArr.map((item, index) => {return <a href="./" key={index} className={style.pages}>{item}</a>})} */}
+        {props.pages.map((item, index) => {return <Link to={`/${item}`} key={index} className={style.pages}>{item}</Link>})}
       </div>
     );
   }
